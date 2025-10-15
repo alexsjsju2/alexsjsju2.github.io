b99ee00ff953cfed2cee68b6299ed76c9ebb9680
alexsjsju2 <alexsjsju0@gmail.com>
2025-10-15 14:39:54 +0200

    Update _initFirebase.js

--- api/_initFirebase.js
+++ api/_initFirebase.js
@@ -1,23 +1,20 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
+import { getAuth } from "firebase-admin/auth";
 
+let adminApp = null;
 let db = null;
 
 export function initFirebase() {
-  if (db) return db;
+  if (adminApp) return { db, auth: adminApp.auth() };
   if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
     throw new Error("Missing FIREBASE_SERVICE_ACCOUNT env var");
   }
   const app = initializeApp({
     credential: cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)),
   });
+  adminApp = app;
   db = getFirestore(app);
-  return db;
-}
-
-export function verifySecret(req) {
-  const clientSecret = req.headers["authorization"];
-  if (!clientSecret || clientSecret !== `Bearer ${process.env.API_SECRET}`) {
-    throw new Error("Unauthorized");
-  }
+  const auth = getAuth(app);
+  return { db, auth };
 }
