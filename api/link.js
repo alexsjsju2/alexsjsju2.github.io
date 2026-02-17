b04b933c675546db4b36ef32e2a7c40c2586cf7c
alexsjsju2 <alexsjsju0@gmail.com>
2026-02-17 17:00:11 +0100

    Update link.js

--- api/link.js
+++ api/link.js
@@ -14,12 +14,10 @@ try {
 }
 
 export default async function handler(req, res) {
-  // CORS headers
   res.setHeader('Access-Control-Allow-Origin', 'https://www.alexsjsju.eu');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  // Gestione preflight OPTIONS
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
