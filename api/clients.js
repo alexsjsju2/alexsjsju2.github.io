9971fbcec0975e673c2a503c344acd844c3e397b
alexsjsju2 <atm194@proton.me>
2026-08-04 12:41:05 +0200

    Update clients.js

--- api/clients.js
+++ api/clients.js
@@ -10,7 +10,7 @@ db=admin.firestore()
 console.error('Errore Firebase:',error)
 }
 export default async function handler(req,res){
-res.setHeader('Access-Control-Allow-Origin','https://www.alexsjsju.eu')
+res.setHeader('Access-Control-Allow-Origin','https://www.alextools.online')
 res.setHeader('Access-Control-Allow-Methods','GET, POST, DELETE, OPTIONS')
 res.setHeader('Access-Control-Allow-Headers','Content-Type')
 if(req.method==='OPTIONS')return res.status(200).end()
