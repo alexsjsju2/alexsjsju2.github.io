a76e5c593f706a37147ae27122982bfb100a40e6
alexsjsju2 <atm194@proton.me>
2026-08-04 12:38:28 +0200

    Update linksp.js

--- api/linksp.js
+++ api/linksp.js
@@ -14,7 +14,7 @@ try {
 }
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://www.alexsjsju.eu');
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.alextools.online');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
