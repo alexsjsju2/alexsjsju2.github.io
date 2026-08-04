97c4b50090a312d0707059c8adab42c2fd55ab80
alexsjsju2 <atm194@proton.me>
2026-08-04 12:37:31 +0200

    Update predict.js

--- api/predict.js
+++ api/predict.js
@@ -17,7 +17,7 @@ const AVAILABLE_MODELS={
 };
 
 function setSecurityHeaders(res){
-  res.setHeader("Access-Control-Allow-Origin","https://www.alexsjsju.eu");
+  res.setHeader("Access-Control-Allow-Origin","https://www.alextools.online");
   res.setHeader("Access-Control-Allow-Methods","GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers","Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials","true");
